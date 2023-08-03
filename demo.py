195f82a68c425059d61cd5b5b0db5aed03b8cf48
Urvi <urvigarg27@gmail.com>
2023-08-03 23:57:53 +0530

    basic python codes

---
+++ demo.py
@@ -0,0 +1,23 @@
+print("my first program")
+print("hlo",6,7,sep="~" , end="009")
+
+# a=1
+# print(a)
+
+a=complex(8,2)
+b=True
+c="urvi"
+d=None
+print("the type of a", type(a))
+print("the type of b" ,type(b))
+print("the type of c" ,type(c))
+print("the type of d", type(d))
+
+list1=[8,2.3,[-4,5],["apple","banana"]]
+print(list1)
+
+tuple1=(("tiger","lion"),("parrot","sparrow"))
+print(tuple1)
+
+dict1={"Name":"sakshi","age":20,"canvote":True}
+print(dict1)
